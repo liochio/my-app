957cf7cc618eaa016f02f7ac28418b7a23dcd4fb
liochio <60186950+liochio@users.noreply.github.com>
2023-10-24 21:24:08 +0700

    comment hello

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 import GlobalStyles from './components/GlobalStyles';
 
+//hello
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
